4f30d5681bb607dca7617ee9259251973c579762
Leon Zeidler <leon@artisancms.app>
2025-11-12 22:30:22 +0100

    vercel changes

--- next.config.js
+++ next.config.js
@@ -1,40 +1,30 @@
-
-
 // Injected content via Sentry wizard below
+const { withSentryConfig } = require('@sentry/nextjs');
 
-const { withSentryConfig } = require("@sentry/nextjs");
+const nextConfig = {
+  // Your normal Next.js config here
+  sentry: {
+    disableServerWebpackPlugin: true,
+    disableClientWebpackPlugin: true,
+    telemetry: false,
+  },
+};
 
-module.exports = withSentryConfig(
-  module.exports,
-  {
-    // For all available options, see:
-    // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+// Optional Sentry plugin settings (currently not used since disabled above)
+const sentryWebpackPluginOptions = {
+  org: "artisancms",
+  project: "javascript-nextjs",
 
-    org: "artisancms",
-    project: "javascript-nextjs",
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
 
-    // Only print logs for uploading source maps in CI
-    silent: !process.env.CI,
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+  widenClientFileUpload: true,
+  tunnelRoute: "/monitoring",
+  disableLogger: true,
+  automaticVercelMonitors: true,
+};
 
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
-
-    // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-    // This can increase your server load as well as your hosting bill.
-    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-    // side errors will fail.
-    tunnelRoute: "/monitoring",
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-
-    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
-    automaticVercelMonitors: true,
-  }
-);
+// Disable Sentry build-time upload (no CLI runs, avoids invalid token errors)
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
